d6dab39256ce0808d5310f608c060d0cd097373c
Sean Moore <sean.moore@valor-software.com>
2021-04-13 17:35:47 +0300

    add saucelabs debugging

--- wdio.conf.js
+++ wdio.conf.js
@@ -26,7 +26,7 @@ exports.config = {
     // directory is where your package.json resides, so `wdio` will be called from there.
     //
     specs: [
-         './e2e/wdio/platform/**/dummy.e2e-spec.ts',
+         './e2e/wdio/**/*.e2e-spec.ts',
     ],
     // Patterns to exclude.
     exclude: [
@@ -134,6 +134,7 @@ exports.config = {
             'sauce:options': {
                 screenResolution: '1920x1080',
                 name: 'e2e-win-chrome ' + process.env.TRAVIS_BUILD_ID + ' ' + process.env.TRAVIS_PULL_REQUEST_BRANCH,
+                extendedDebugging: true,
             }
         },
         // {
